58e4f7b8c1e28f8a98c7c4a97c63b8a4dbee44ce
Enrique Phillips <enrique.phillips.wac@gmail.com>
2010-07-25 10:02:27 +0200

    Initial commit with a straight Rails 3.0.0beta4 + lib/tasks/jquery_setup.rake

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '0dee4560bdcf8994cda8fa2cca826b56f7409c3939ae3f4a96b2989f8a605f635232afe29cf1318320c09d989af5dd23a539c54ff8ab06642418bed1cbcaae08'
